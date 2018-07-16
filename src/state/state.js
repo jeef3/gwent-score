518a900ce87fa49acf96db7e61b807104f0103e6
Jeff Knaggs <mail@jeef3.com>
2018-07-16 22:42:40 +1200

    Remove fake data

--- src/state/state.js
+++ src/state/state.js
@@ -1,42 +1,4 @@
 export default {
   showModal: false,
-  cards: [
-    {
-      player: 'a',
-      hero: false,
-      points: 5,
-      attrs: 'muster',
-      combat: 'close'
-    },
-    {
-      player: 'a',
-      hero: false,
-      points: 5,
-      attrs: 'muster',
-      combat: 'close'
-    },
-    {
-      player: 'a',
-      hero: false,
-      points: null,
-      attrs: null,
-      combat: 'close',
-      special: 'commander-horn'
-    },
-    {
-      player: null,
-      hero: false,
-      points: null,
-      attrs: null,
-      combat: 'close',
-      special: 'weather'
-    },
-    {
-      player: 'b',
-      hero: true,
-      points: 15,
-      attrs: null,
-      combat: 'close'
-    }
-  ]
+  cards: []
 };

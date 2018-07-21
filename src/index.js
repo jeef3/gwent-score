3ec8b2e5dd958354e04d7fb7b46d6fe7e05d570e
Jeff Knaggs <mail@jeef3.com>
2018-07-22 09:47:52 +1200

    Put service worker back in

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import injectGlobalStyles from './injectGlobalStyles';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 injectGlobalStyles();
+registerServiceWorker();

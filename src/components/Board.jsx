ca73db9df11733f9d27e54c0a62c5dbfe3b49246
Jeff Knaggs <mail@jeef3.com>
2018-07-22 22:25:41 +1200

    Adding some style to the board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,44 +3,85 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 import { Actions, Selector } from '../state';
+import woodTexture from '../assets/cherry4.jpg';
+import CombatRowBackground from './molecules/CombatRowBackground';
+import Plus from './atoms/icons/Plus';
 import Card from './Card';
 
-const Row = styled.div`
-  margin: 0 0 5px;
+const Container = styled.div`
+  background-image: url(${woodTexture});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+Container.displayName = 'Container';
 
-  border: solid 1px lightgray;
+const Row = styled.div`
+  background-image: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0.2),
+    rgba(255, 255, 255, 0) 5%,
+    rgba(0, 0, 0, 0) 75%,
+    rgba(0, 0, 0, 0.7)
+  );
 
   display: grid;
   grid-template-columns: 1fr 50px 50px;
   grid-template-rows: 50px 1fr;
   grid-template-areas:
-    'name name name'
-    'cards score add';
+    'name score add'
+    'cards cards cards';
 `;
 Row.displayName = 'Row';
 
 const RowName = styled.h2`
+  padding: 0 10px;
   margin: 0;
+
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
 
   grid-area: name;
   align-self: center;
 `;
 RowName.displayName = 'RowName';
+
 const CombatRow = styled.div`
+  position: relative;
+  min-height: 70px;
+  padding: 0 8px;
+
   display: flex;
+  flex-wrap: wrap;
 
   grid-area: cards;
 `;
 CombatRow.displayName = 'CombatRow';
 
 const Add = styled.button`
+  padding: 13px;
+
   cursor: pointer;
+  color: white;
+
+  border: 0;
+
+  background: transparent;
+
   grid-area: add;
 `;
 Add.displayName = 'Add';
 
 const Score = styled.h2`
+  align-self: center;
+
   margin: 0;
+
+  color: white;
+  font-family: 'Gwent', sans-serif;
+  font-size: 24;
+
+  text-align: center;
 
   grid-area: score;
 `;
@@ -77,70 +118,92 @@ export default connect(
   })
 )(({ board, onCardClick, playerA, playerB }) => (
   <Scroller>
-    <div>
-      <Row>
-        <RowName>Siege</RowName>
-        <CombatRow>
-          {board.playerA.siege.cards.map(c => (
-            <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
-          ))}
-        </CombatRow>
-        <Add onClick={playerA.onPlaySiege}>+</Add>
-        <Score>{board.playerA.siege.score}</Score>
-      </Row>
-      <Row>
-        <RowName>Ranged</RowName>
-        <CombatRow>
-          {board.playerA.ranged.cards.map(c => (
-            <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
-          ))}
-        </CombatRow>
-        <Add onClick={playerA.onPlayRanged}>+</Add>
-        <Score>{board.playerA.ranged.score}</Score>
-      </Row>
-      <Row>
-        <RowName>Close</RowName>
-        <CombatRow>
-          {board.playerA.close.cards.map(c => (
-            <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
-          ))}
-        </CombatRow>
-        <Add onClick={playerA.onPlayClose}>+</Add>
-        <Score style={{ gridArea: 'score' }}>{board.playerA.close.score}</Score>
-      </Row>
-    </div>
-    <hr />
-    <div>
-      <Row>
-        <RowName>Close</RowName>
-        <CombatRow>
-          {board.playerB.close.cards.map(c => (
-            <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
-          ))}
-        </CombatRow>
-        <Add onClick={playerB.onPlayClose}>+</Add>
-        <Score>{board.playerB.close.score}</Score>
-      </Row>
-      <Row>
-        <RowName>Ranged</RowName>
-        <CombatRow>
-          {board.playerB.ranged.cards.map(c => (
-            <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
-          ))}
-        </CombatRow>
-        <Add onClick={playerB.onPlayRanged}>+</Add>
-        <Score>{board.playerB.ranged.score}</Score>
-      </Row>
-      <Row>
-        <RowName>Siege</RowName>
-        <CombatRow>
-          {board.playerB.siege.cards.map(c => (
-            <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
-          ))}
-        </CombatRow>
-        <Add onClick={playerB.onPlaySiege}>+</Add>
-        <Score>{board.playerB.siege.score}</Score>
-      </Row>
-    </div>
+    <Container>
+      <div>
+        <Row>
+          <RowName>Siege</RowName>
+          <CombatRow>
+            <CombatRowBackground combat="siege" />
+            {board.playerA.siege.cards.map(c => (
+              <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
+            ))}
+          </CombatRow>
+          <Add onClick={playerA.onPlaySiege}>
+            <Plus />
+          </Add>
+          <Score>{board.playerA.siege.score}</Score>
+        </Row>
+        <Row>
+          <RowName>Ranged</RowName>
+          <CombatRow>
+            <CombatRowBackground combat="ranged" />
+            {board.playerA.ranged.cards.map(c => (
+              <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
+            ))}
+          </CombatRow>
+          <Add onClick={playerA.onPlayRanged}>
+            <Plus />
+          </Add>
+          <Score>{board.playerA.ranged.score}</Score>
+        </Row>
+        <Row>
+          <RowName>Close</RowName>
+          <CombatRow>
+            <CombatRowBackground combat="close" />
+            {board.playerA.close.cards.map(c => (
+              <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
+            ))}
+          </CombatRow>
+          <Add onClick={playerA.onPlayClose}>
+            <Plus />
+          </Add>
+          <Score style={{ gridArea: 'score' }}>
+            {board.playerA.close.score}
+          </Score>
+        </Row>
+      </div>
+      <hr />
+      <div>
+        <Row>
+          <RowName>Close</RowName>
+          <CombatRow>
+            <CombatRowBackground combat="close" />
+            {board.playerB.close.cards.map(c => (
+              <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
+            ))}
+          </CombatRow>
+          <Add onClick={playerB.onPlayClose}>
+            <Plus />
+          </Add>
+          <Score>{board.playerB.close.score}</Score>
+        </Row>
+        <Row>
+          <RowName>Ranged</RowName>
+          <CombatRow>
+            <CombatRowBackground combat="ranged" />
+            {board.playerB.ranged.cards.map(c => (
+              <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
+            ))}
+          </CombatRow>
+          <Add onClick={playerB.onPlayRanged}>
+            <Plus />
+          </Add>
+          <Score>{board.playerB.ranged.score}</Score>
+        </Row>
+        <Row>
+          <RowName>Siege</RowName>
+          <CombatRow>
+            <CombatRowBackground combat="siege" />
+            {board.playerB.siege.cards.map(c => (
+              <Card key={c.id} {...c} onClick={() => onCardClick(c)} />
+            ))}
+          </CombatRow>
+          <Add onClick={playerB.onPlaySiege}>
+            <Plus />
+          </Add>
+          <Score>{board.playerB.siege.score}</Score>
+        </Row>
+      </div>
+    </Container>
   </Scroller>
 ));
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,21 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  box-sizing: border-box;
+  width: 40px;
+  height: 60px;
   padding: 2px;
-  margin: 2px;
+  margin: 5px;
 
   cursor: pointer;
 
-  border: solid 1px black;
+  text-align: center;
+
+  border: solid 2px ${({ hero }) => (hero ? '#CA863A' : 'transparent')};
+  border-radius: 5px;
 
   background: lightgray;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
 `;
 Container.displayName = 'Card_Container';
 
-export default ({ points, score, attrs, onClick = () => {} }) => (
-  <Container onClick={onClick}>
-    <div style={{ fontSize: 18, color: 'green' }}>{score}</div>
-    <div style={{ fontSize: 11, color: 'gray' }}>{points}</div>
+export default ({ hero, points, score, attrs, onClick = () => {} }) => (
+  <Container hero={hero} onClick={onClick}>
+    {score === points ? (
+      <div style={{ fontSize: 18, color: 'black' }}>{score}</div>
+    ) : (
+      <React.Fragment>
+        <div style={{ fontSize: 18, color: 'green' }}>{score}</div>
+        <div style={{ fontSize: 11, color: 'gray' }}>{points}</div>
+      </React.Fragment>
+    )}
+
     <div>{attrs}</div>
   </Container>
 );

552edbffddc065f91a643e94c72c98dc551f985e
Jeff Knaggs <mail@jeef3.com>
2018-08-06 23:48:43 +1200

    Remove fake cards

--- src/state/state.js
+++ src/state/state.js
@@ -3,40 +3,7 @@ export default {
   dialog: null,
   dialogData: null,
 
-  cards: [
-    {
-      id: 1,
-      player: 'a',
-      points: 5,
-      combat: 'close'
-    },
-    {
-      id: 2,
-      player: 'a',
-      points: 4,
-      combat: 'ranged'
-    },
-    {
-      id: 3,
-      player: 'a',
-      points: 4,
-      combat: 'close'
-    },
-    {
-      id: 4,
-      player: 'b',
-      points: 8,
-      combat: 'siege',
-      attr: 'tight-bond'
-    },
-    {
-      id: 5,
-      player: 'b',
-      points: 8,
-      combat: 'siege',
-      attr: 'tight-bond'
-    }
-  ],
+  cards: [],
 
   players: {
     playerA: {

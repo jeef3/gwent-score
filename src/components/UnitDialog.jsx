acbd10d613bbf74b41c50e73c02c8c95c6f50287
Jeff Knaggs <mail@jeef3.com>
2018-08-26 21:52:02 +1200

    Remove points 11-14 as they aren't used yet

--- src/components/UnitDialog.jsx
+++ src/components/UnitDialog.jsx
@@ -65,7 +65,7 @@ const InnerForm = ({ values, handleChange, handleSubmit, setFieldValue }) => (
         <ItemSelect
           downButton={minus}
           upButton={plus}
-          items={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]}
+          items={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]}
           value={values.points}
           renderValue={() => (
             <PointRing
